cbe3ff71f11e9026ab5daa4c149a4a4e8b05aca6
Lakifume <56451477+Lakifume@users.noreply.github.com>
2024-05-04 17:47:20 -0500

    Add files via upload

---
+++ SnippetSrc/DetachedObjectPropertyData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UAssetAPI.PropertyTypes.Objects;
+using UAssetAPI.UnrealTypes;
+
+namespace UAssetSnippet;
+
+public class DetachedObjectPropertyData : PropertyData<int>
+{
+    private static readonly FString _currentPropertyType = new("DetachedObjectProperty");
+
+    public List<int> UsedExports = default;
+    public override FString PropertyType => _currentPropertyType;
+    public DetachedObjectPropertyData(FName name, List<int> usedExports) : this(name)
+    {
+        UsedExports = usedExports;
+    }
+
+    public DetachedObjectPropertyData(FName name) : base(name) { }
+
+    public DetachedObjectPropertyData() { }
+}
